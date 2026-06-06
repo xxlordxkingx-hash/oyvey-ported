32736343996473a34e26759ed022a69e3644f2dd
xxlordxkingx-hash <xxlordxkingx@gmail.com>
2026-06-06 13:28:52 -0400

    Update ModuleManager.java

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -14,7 +14,7 @@ import me.alpha432.oyvey.features.modules.client.NotificationsModule;
 import me.alpha432.oyvey.features.modules.combat.CriticalsModule;
 import me.alpha432.oyvey.features.modules.combat.KeyPearlModule;
 import me.alpha432.oyvey.features.modules.hud.CoordinatesHudModule;
-import me.alpha432.oyvey.features.modules.hud.WatermarkHudModule;
+// import me.alpha432.oyvey.features.modules.hud.WatermarkHudModule;
 import me.alpha432.oyvey.features.modules.misc.MCFModule;
 import me.alpha432.oyvey.features.modules.movement.ReverseStepModule;
 import me.alpha432.oyvey.features.modules.movement.StepModule;
@@ -37,7 +37,7 @@ public class ModuleManager implements Jsonable, Util {
     private final List<Module> modules = new ArrayList<>();
 
     public void init() {
-        register(new WatermarkHudModule());
+        // register(new WatermarkHudModule());
         register(new CoordinatesHudModule());
         register(new HudEditorModule());
         register(new ClickGuiModule());
