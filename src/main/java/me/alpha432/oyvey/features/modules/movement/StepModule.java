b44917d31fea00c1c1dc37fb6bb3d7acd6d61a6e
xxlordxkingx-hash <xxlordxkingx@gmail.com>
2026-06-06 13:22:40 -0400

    Update StepModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/movement/StepModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/StepModule.java
@@ -8,7 +8,7 @@ public class StepModule extends Module {
     private final Setting<Float> height = num("Height", 2f, 1f, 3f);
 
     public StepModule() {
-        super("FastUpStairs", "step..", Category.MOVEMENT);
+        super("FastUpStairs", "Instantly Jumps Up The Stairs Fast:)", Category.MOVEMENT);
     }
 
     private float prev;
