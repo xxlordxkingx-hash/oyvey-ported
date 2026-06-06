87438124efaff84259df80e750c230928a391cc1
xxlordxkingx-hash <xxlordxkingx@gmail.com>
2026-06-06 13:37:37 -0400

    Update FastPlaceModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/FastPlaceModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/FastPlaceModule.java
@@ -1,19 +1,16 @@
 package me.alpha432.oyvey.features.modules.player;
 
 import me.alpha432.oyvey.features.modules.Module;
-import net.minecraft.world.item.Items;
 
 public class FastPlaceModule extends Module {
     public FastPlaceModule() {
-        super("FastPlace", "Makes you throw exp faster", Category.PLAYER);
+        super("FastPlace", "Removes right-click delay", Category.PLAYER);
     }
 
     @Override
     public void onTick() {
         if (nullCheck()) return;
 
-        if (mc.player.isHolding(Items.EXPERIENCE_BOTTLE)) {
-            mc.rightClickDelay = 0;
-        }
+        mc.rightClickDelay = 0;
     }
 }
